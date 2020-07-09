61b8a8b8eaac3f5022a5493801556360b4f3f0a0
Metallicode <41553615+Metallicode@users.noreply.github.com>
2020-07-09 21:43:58 +0300

    RMS_NOISE

--- 18_noise_gate.py
+++ 18_noise_gate.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from scipy.signal import butter,filtfilt
+from modifier import Modifier
 
 #normalize function
 def norm(data):
@@ -12,11 +13,18 @@ def norm(data):
     data = np.array([((x-min_v) / (max_v-min_v)) for x in data])*2.0-1
     return data * ((max_v/min_v)*-1)
 
+def gates(signal, segments):
+    m = Modifier(50)
+    ones = np.ones_like(signal)
+    for s in segments:
+        ones[s[0]:s[1]] = m.modifier(s)        
+    return ones
+
 def segments(keyframes, First=True):
     if First == True:
         return [(keyframes[i],keyframes[i+1]) for i in range(0,len(keyframes)-1,2)]
     else:
-        return [(keyframes[i],keyframes[i+1]) for i in range(1,len(keyframes)-1,2)]
+        return [(keyframes[i],keyframes[i+1]) for i in range(1,len(keyframes)-1, 2)]
 
 #read file
 samplerate, signal = wavfile.read("count.wav")
@@ -37,7 +45,7 @@ b, a = butter(2, normal_cutoff, btype="low", analog=False)
 rms = filtfilt(b, a,rms)
 
 #keyframes 
-threshold = 0.005
+threshold = 0.02
 keyframes = []
 lastkeyframewasUp = False
 
@@ -53,20 +61,22 @@ for i in range(len(rms)):
 
 keyframes.append(len(signal))
 
+
+
+gated = signal*gates(signal, segments(keyframes, False))
+
 #draw
 plt.plot(range(len(signal)), signal, "black")
-plt.plot(range(len(rms)), rms, "red")
+plt.plot(range(len(signal)), gated, "pink")
 
+plt.plot(range(len(signal)), gates(signal, segments(keyframes, False)), "yellow")
+plt.plot(range(len(rms)), rms, "red")
 plt.axhline(y=threshold, color ="green")
 
-for i in range(len(keyframes)):
-    plt.axvline(x=keyframes[i], color ="yellow")
-
-for v in segments(keyframes):
-    plt.axvspan(v[0],v[1], facecolor='0.2', alpha=0.5)
-
-for v in segments(keyframes, False):
-    plt.axvspan(v[0],v[1], facecolor='0.5', alpha=0.2)
-
-
 plt.show()
+
+gated *= 32767
+gated = np.int16(gated)
+wavfile.write("file.wav", 44100, gated)
+
+
