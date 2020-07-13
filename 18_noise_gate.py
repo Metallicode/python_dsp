6a1ef2fb574cc65fce2e647e2561745e0b0e9d40
Metallicode <41553615+Metallicode@users.noreply.github.com>
2020-07-13 18:24:38 +0300

    hyper sin insted of modifier

--- 18_noise_gate.py
+++ 18_noise_gate.py
@@ -1,24 +1,27 @@
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from scipy.signal import butter,filtfilt
-from modifier import Modifier
 
 #normalize function
-def norm(data):
+def norm(data, simple=False):
     min_v = min(data)
     max_v = max(data)
-    offset = min_v+max_v
-    data = data+(offset/2)
-    data = np.array([((x-min_v) / (max_v-min_v)) for x in data])*2.0-1
-    return data * ((max_v/min_v)*-1)
+    if simple is False:
+        offset = min_v+max_v
+        data = data+(offset/2)
+        data = np.array([((x-min_v) / (max_v-min_v)) for x in data])*2.0-1
+        return data * ((max_v/min_v)*-1)
+    else:
+        return np.array([((x-min_v) / (max_v-min_v)) for x in data]) 
 
-def gates(signal, segments):
-    m = Modifier(50)
+def gates(signal, segments, speed= 50):
     ones = np.ones_like(signal)
     for s in segments:
-        ones[s[0]:s[1]] = m.modifier(s)        
+        t = np.linspace(-speed,speed,s[1]-s[0])
+        hyper_s = np.sinh(t)
+        window = (hyper_s*hyper_s)
+        ones[s[0]:s[1]] = norm(window, True)
     return ones
 
 def segments(keyframes, First=True):
@@ -26,7 +29,6 @@ def segments(keyframes, First=True):
         return [(keyframes[i],keyframes[i+1]) for i in range(0,len(keyframes)-1,2)]
     else:
         return [(keyframes[i],keyframes[i+1]) for i in range(1,len(keyframes)-1, 2)]
-
 
 def calculate_dynamics(rms, threshold):
     keyframes = []
@@ -45,6 +47,9 @@ def calculate_dynamics(rms, threshold):
     keyframes.append(len(signal))
     return keyframes
     
+
+
+
 
 
 #read file
@@ -68,13 +73,14 @@ rms = filtfilt(b, a,rms)
 #gate
 threshold = 0.2
 keyframes = calculate_dynamics(rms, threshold)
-gated = signal*gates(signal, segments(keyframes, False))
+g = gates(signal, segments(keyframes, False), 50)
+gated = signal*g
 
 #draw
 plt.plot(range(len(signal)), signal, "black")
 plt.plot(range(len(signal)), gated, "pink")
 
-plt.plot(range(len(signal)), gates(signal, segments(keyframes, False)), "yellow")
+plt.plot(range(len(signal)), g, "yellow")
 plt.plot(range(len(rms)), rms, "red")
 plt.axhline(y=threshold, color ="green")
 
