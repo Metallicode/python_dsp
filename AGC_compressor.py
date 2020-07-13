08d4771065f1a0eefe09b7a873609ff3e4184a7b
Metallicode <41553615+Metallicode@users.noreply.github.com>
2020-07-13 19:54:06 +0300

    Update AGC_compressor.py

--- AGC_compressor.py
+++ AGC_compressor.py
@@ -2,23 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.io import wavfile
 from scipy.signal import butter,filtfilt
-from modifier import Modifier
 
 #normalize function
-def norm(data):
+def norm(data, simple=False):
     min_v = min(data)
     max_v = max(data)
-    offset = min_v+max_v
-    data = data+(offset/2)
-    data = np.array([((x-min_v) / (max_v-min_v)) for x in data])*2.0-1
-    return data * ((max_v/min_v)*-1)
+    if simple is False:
+        offset = min_v+max_v
+        data = data+(offset/2)
+        data = np.array([((x-min_v) / (max_v-min_v)) for x in data])*2.0-1
+        return data * ((max_v/min_v)*-1)
+    else:
+        return np.array([((x-min_v) / (max_v-min_v)) for x in data]) 
 
 def segments(keyframes, First=True):
     if First == True:
         return [(keyframes[i],keyframes[i+1]) for i in range(0,len(keyframes)-1,2)]
     else:
         return [(keyframes[i],keyframes[i+1]) for i in range(1,len(keyframes)-1,2)]
-
 
 def calculate_dynamics(rms, threshold):
     keyframes = [0]
@@ -37,6 +38,11 @@ def calculate_dynamics(rms, threshold):
     keyframes.append(len(signal))
     return keyframes
     
+
+
+
+
+
 
 #read file
 samplerate, signal = wavfile.read("beat.wav")
@@ -60,32 +66,35 @@ rms = filtfilt(b, a,rms)
 threshold = 0.05
 keyframes = calculate_dynamics(rms, threshold)
 
+
 #commpression power
-ratio = 1.5
+ratio = 1.2
+bias = -0.2
+speed = 80
+makeup_gain =1
 
 min_rms = min(rms)
 
+c = np.zeros_like(rms)
+gr = np.zeros_like(rms)
+
 compression = (-rms*ratio)+(1-(min_rms+threshold))
 
-c = np.zeros_like(compression)
-gr = np.zeros_like(compression)
-
-m = Modifier(5)
 for segment in segments(keyframes, False):
-    gr[segment[0]:segment[1]] = rms[segment[0]:segment[1]]*(m.modifier(segment)*-1+1)
+    gr[segment[0]:segment[1]] = ((rms[segment[0]:segment[1]]-bias)*(-1*norm(np.cosh(np.linspace(-speed,speed,segment[1]-segment[0])), True)+1))
     for i in range(segment[0],segment[1],buffer_size):
         s = gr[i:i+buffer_size]*-1+1
-        c[i:i+buffer_size] = signal[i:i+buffer_size] *s
+        c[i:i+buffer_size] = signal[i:i+buffer_size] * s
 
-   
 for segment in segments(keyframes):
     for i in range(segment[0],segment[1],buffer_size):
         c[i:i+buffer_size] = signal[i:i+buffer_size]
 
-
-makeup_gain =1
-
 c = norm(c)*makeup_gain
+
+
+
+
 
 #draw
 plt.plot(range(len(signal)), signal, "black")
@@ -108,6 +117,8 @@ for v in segments(keyframes, False):
 plt.show()
 
 
+
+#write file
 c *= 32767
 c = np.int16(c)
 wavfile.write("file.wav", 44100, c)
